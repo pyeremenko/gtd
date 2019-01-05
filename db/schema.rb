3fafec36f1946d7775161e397571cfea30bfcd65
Peter Yeremenko <peter.yeremenko@toptal.com>
2019-01-05 07:47:56 +0600

    add duration to Event

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_04_032020) do
+ActiveRecord::Schema.define(version: 2019_01_05_013013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_01_04_032020) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.string "scheduled_at", limit: 5
+    t.integer "duration"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
